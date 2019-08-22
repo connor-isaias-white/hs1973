1344620286397a1e93918b0eb866dc48939301e3
connor <connor.white.spam@gmail.com>
2019-08-22 16:46:03 +1000

    basics

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const http = require('http');
+const formidable = require('formidable');
+var fs = require('fs');
 
 app.get('/', function(req, res) {
-    res.sendfile("index.html");
+    res.sendfile("mcmods.html");
+});
+
+app.listen(3000, function() {
+    console.log('listening on port 3000');
 });
